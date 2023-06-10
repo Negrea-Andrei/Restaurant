959683fa2f65b60e1e84a10a4e5b0176d3900ef5
Negrea-Andrei <negrea97@yahoo.com>
2023-06-10 14:34:59 +0300

    Add npm wath to the script

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,9 @@
-console.log("Working");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Wo   rk")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHNCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIldvICAgcmtcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
