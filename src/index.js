430b03759008c8580cbb6d7a6e1de492a1a08dd4
Negrea-Andrei <negrea97@yahoo.com>
2023-06-13 19:35:23 +0300

    Used webpack to create the homepage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './home.css'
 
-const background = document.createElement('div');
-background.classList.add('content')
+const background = document.querySelector('.content');
 
 const text = document.createElement('div');
 text.innerHTML= "Welcome to <br>Odin's seafood restaurant!"
 text.classList.add('welcome');
 
 background.appendChild(text);
-document.body.appendChild(background)
