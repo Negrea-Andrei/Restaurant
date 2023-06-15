c1ba1a9a3509d4f88d14b7be6e441cd3ec1140fc
Negrea-Andrei <negrea97@yahoo.com>
2023-06-15 21:04:22 +0300

    Resize the about section cards

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import about from './about';
 import home from './home';
-const background = document.querySelector('.content');
 
 home()
 
-function test() {
-    about()
-}
-
 let aboutButton = document.querySelector('.about_section');
-aboutButton.addEventListener('click', test)
+aboutButton.addEventListener('click', about)
