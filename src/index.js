561ebb1f6f10ed1061275c2dca56f8fd417241d3
Negrea-Andrei <negrea97@yahoo.com>
2023-06-16 20:32:09 +0300

    Create the Home button and ad the functionality to it

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import about from './about';
 import home from './home';
 
-const background = document.querySelector('.content');
-background.innerHTML = '';
-background.style.display = 'flex';
-background.style.background = `url(D:\HTML\Restaurant\dist\b2193088b5c3438a025f.jpeg) center/cover`;
-
-const text = document.createElement('div');
-text.innerHTML = "Welcome to <br>Odin's seafood restaurant!"
-text.className = "welcome";
-
-background.appendChild(text);
+home()
 
 let aboutButton = document.querySelector('.about_section');
 aboutButton.addEventListener('click', about);
